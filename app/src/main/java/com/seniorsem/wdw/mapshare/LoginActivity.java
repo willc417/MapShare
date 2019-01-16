50a2af1b46045017a41c26de7288dccddc28e00b
Will C <willc417@gmail.com>
2019-01-15 20:47:35 -0600

    Included Butterknife (needed to use an older version), set up Firebase authentication

--- app/src/main/java/com/seniorsem/wdw/mapshare/LoginActivity.java
+++ app/src/main/java/com/seniorsem/wdw/mapshare/LoginActivity.java
@@ -1,39 +1,166 @@
 package com.seniorsem.wdw.mapshare;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.username_edit)
+    EditText etEmail;
+    @BindView(R.id.password_edit)
+    EditText etPassword;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        ButterKnife.bind(this);
 
-        Button btn_login = findViewById(R.id.login_btn);
-        btn_login.setOnClickListener(new View.OnClickListener() {
+    }
+    @OnClick(R.id.login_btn)
+    void loginClick() {
+        if (!isFormValid()) {
+            return;
+        }
+
+        showProgressDialog();
+
+
+
+        firebaseAuth.signInWithEmailAndPassword(
+                etEmail.getText().toString(),
+                etPassword.getText().toString()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                hideProgressDialog();
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+                if (task.isSuccessful()) {
+                    Intent intentMain = new Intent();
+                    intentMain.setClass(LoginActivity.this, MainActivity.class);
+                    Log.d("TAG_UI", "HERE");
+                    startActivity(intentMain);
+                } else {
+                    Toast.makeText(LoginActivity.this,
+                            "Error: "+task.getException().getMessage(),
+                            Toast.LENGTH_SHORT).show();
 
-                Intent nextActivity = new Intent();
-                nextActivity.setClass(LoginActivity.this, MainActivity.class);
-                startActivity(nextActivity);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+
+            }
+        });
+    }
+
+    @OnClick(R.id.register_btn)
+    void registerClick() {
+        if (!isFormValid()) {
+            return;
+        }
+
+        showProgressDialog();
+        //Register.setProgress(50);
+
+        firebaseAuth.createUserWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            FirebaseUser fbUser = task.getResult().getUser();
+
+                            fbUser.updateProfile(new UserProfileChangeRequest.Builder().
+                                    setDisplayName(usernameFromEmail(fbUser.getEmail())).build());
+
+                            Toast.makeText(LoginActivity.this, "User created", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                hideProgressDialog();
+                e.printStackTrace();
             }
         });
 
     }
 
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+
+        }
+    }
+
+
+    private boolean isFormValid() {
+        if (TextUtils.isEmpty(etEmail.getText().toString())) {
+            etEmail.setError("Required");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(etPassword.getText().toString())) {
+            etPassword.setError("Required");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Loading...");
+        }
+
+        progressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
 
 }
