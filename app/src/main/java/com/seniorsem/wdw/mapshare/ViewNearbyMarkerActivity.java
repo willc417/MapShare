c2cab209f969c0ff6bf84ef6422d1f60c8338a48
Will C <willc417@gmail.com>
2019-03-12 16:34:39 -0500

    Added features to query the database when a nearby marker is clicked. May need to change the logic bc user input could cause the split's to fail.

--- app/src/main/java/com/seniorsem/wdw/mapshare/ViewNearbyMarkerActivity.java
+++ app/src/main/java/com/seniorsem/wdw/mapshare/ViewNearbyMarkerActivity.java
@@ -1,15 +1,53 @@
 package com.seniorsem.wdw.mapshare;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.seniorsem.wdw.mapshare.data.Map;
+import com.seniorsem.wdw.mapshare.data.MyMarker;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ViewNearbyMarkerActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.nearbyMarkerTitle)
+    TextView markerTitle;
+
+    @BindView(R.id.nearbyMarkerDesc)
+    TextView nearbyMarkerDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_nearby_marker);
 
+        ButterKnife.bind(this);
 
+
+        Intent thisIntent = getIntent();
+        Bundle b = thisIntent.getExtras();
+        String mapName = b.getString("MapName");
+        final int markerNum = b.getInt("MarkerNum");
+
+
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("createdMaps").document(mapName).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Map newMap = documentSnapshot.toObject(Map.class);
+                MyMarker thisMarker = newMap.getMyMarkers().get(markerNum);
+
+                markerTitle.setText(thisMarker.getTitle());
+                nearbyMarkerDesc.setText(thisMarker.getDescription());
+            }
+        });
     }
 }
